46a79107328459f8a6513ed8f5432ae7aa66bc2f
Varvara008 <vrumyantsevy@gmail.com>
2025-06-10 16:25:23 +0300

    Проверка на абсолютные пути (exe)

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import eel, base64, os
+import eel, base64, os, sys
 from predict import load_model, predict
 from bottle import static_file, route, run
+from get_path import resource_path
 
 eel.init("client")
-model = load_model("defect_model.pth")
+model = load_model(resource_path("defect_model.pth"))
+
+media_dir = os.path.join(os.path.dirname(sys.executable if getattr(sys, 'frozen', False) else __file__), "media")
 
 @eel.expose
 def process_file(file, file_name):
-
+    file_path = os.path.join(media_dir, file_name)
     print("file", file, "name", file_name)
-    with open(f"media/{file_name}", "wb") as f:
+
+    with open(file_path, "wb") as f:
         f.write(base64.b64decode(file))
     
     result = predict(model, f"media/{file_name}")
@@ -18,6 +22,7 @@ def process_file(file, file_name):
 @route("/media/<filename>")
 def serve_media(filename):
     return static_file(filename, root="media")
+
 
 if __name__ == "__main__":
     eel.start("index.html", size=(750, 1000))
